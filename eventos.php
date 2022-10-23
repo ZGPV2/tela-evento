452d6f0a39f42d786588db636de72ddf255e043a
ErickSilva <ericksilva.ifrs@gmail.com>
2022-10-23 19:50:50 -0300

    commit_inicial

---
+++ eventos.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/evento.css">
+    <link rel="stylesheet" href="./css/responsivo-evento.css">
+    <title>Eventos</title>
+</head>
+<body>
+    
+    <header>
+        <main>
+            <div class="menu">
+                <h2>MENU</h1>
+            </div>
+        </main>
+    </header>
+
+    <div class="main-box">
+        <div class="card-eventos">
+
+                <h2>Eventos</h2>
+                <P class="subtitle">"Ninguém te ensinará mais sobre fotografia do que uma criança.
+                    Fotografar crianças é momento, é movimento, é agora. <b class="junction-hidde-works"> A vida também é assim."</b>
+                    <br><b class="junction-show-works"> A vida também é assim."</b>
+                </P>
+
+            <div class="button-group">
+                <button type="button" class="active" data-filter="*" id="btn-all">Todos</button>
+                <button type="button" data-filter=".junina">Festas Juninas</button>
+                <button type="button" data-filter=".jogos">Jogos Escolares</button>
+                <button type="button" data-filter=".alunos">Nossos Alunos</button>
+            </div>
+
+            <div class="container">
+
+                    <div class="element-item junina">
+                        <a href="./img/junina1.jpg" class="popup-link">
+                            <img src="./img/junina1.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Festa Junina</span>
+                    </div>
+
+                    <div class="element-item junina">
+                        <a href="./img/junina2.jpg" class="popup-link">
+                            <img src="./img/junina2.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Festa Junina</span>
+                    </div>
+                    
+                    <div class="element-item junina">
+                        <a href="./img/junina3.jpg" class="popup-link">
+                            <img src="./img/junina3.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Festa Junina</span>
+                    </div>
+
+                    <div class="element-item junina">
+                        <a href="./img/junina4.jpg" class="popup-link">
+                            <img src="./img/junina4.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Festa Junina</span>
+                    </div>
+
+                    <div class="element-item jogos">
+                        <a href="./img/jogos1.jpg" class="popup-link">
+                            <img src="./img/jogos1.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Jogos Escolares</span>
+                    </div>
+
+                    <div class="element-item jogos">
+                        <a href="./img/jogos2.jpg" class="popup-link">
+                            <img src="./img/jogos2.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Jogos Escolares</span>
+                    </div>
+
+                    <div class="element-item jogos">
+                        <a href="./img/jogos3.jpg" class="popup-link">
+                            <img src="./img/jogos3.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Jogos Escolares</span>
+                    </div>
+
+                    <div class="element-item jogos">
+                        <a href="./img/jogos4.jpg" class="popup-link">
+                            <img src="./img/jogos4.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Jogos Escolares</span>
+                    </div>
+
+                    <div class="element-item alunos">
+                        <a href="./img/estudante1.jpg" class="popup-link">
+                            <img src="./img/estudante1.jpg" class="img-fluid"/></a>
+                        <span class="emphasis-name">Alunos</span>
+                    </div>
+
+                    <div class="element-item alunos">
+                        <a href="./img/estudante2.jpg" class="popup-link">
+                            <img src="./img/estudante2.jpg" class="img-fluid" /></a>
+                        <span class="emphasis-name">Alunos</span>
+                    </div>
+
+                    <div class="element-item alunos">
+                        <a href="./img/estudante3.jpg" class="popup-link">
+                            <img src="./img/estudante3.jpg" class="img-fluid" /></a>
+                        <span class="emphasis-name"> Alunos</span>
+                    </div>
+
+                    <div class="element-item alunos">
+                        <a href="./img/estudante4.jpg" class="popup-link">
+                            <img src="./img/estudante4.jpg" class="img-fluid" /></a>
+                        <span class="emphasis-name">Alunos</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+</body>
+</html>
